8ce4c1f00a76672fe823aa54638432436cb2f777
BusinessField <business.field123@gmail.com>
2024-09-17 07:17:18 +0100

    Update5: Header & Blog Page

--- js/pages/blog-page.js
+++ js/pages/blog-page.js
@@ -1,16 +1,36 @@
+// <-- Handle Blog Search -->
+
+let searchInput = document.querySelector(".blog-search .blog-search-inp");
+let searchResultBox = document.querySelector(".search-result");
+
+searchInput.addEventListener("input", () => {
+    if (searchInput.value !== "") {
+        handleBlogSearch();
+    } else {
+        searchResultBox.innerHTML = "";
+    }
+});
+
+
+
 // <-- Set Posts -->
-let filterButtons = document.querySelectorAll(".bullets button");
+
+let filter = document.querySelector(".filter");
+let bulletsHolder = document.querySelector(".bullets");
+let bullets = document.querySelectorAll(".bullets li button");
 let postsHolder = document.querySelector(".posts-holder");
 let postsCategory = "all";
 
 getBlogPosts(postsCategory);
 
 // Control Posts Category + Remove & Add Active Class
-filterButtons.forEach((button) => {
+bullets.forEach((button) => {
     button.addEventListener("click", (e) => {
-        filterButtons.forEach((btn) => {
-            removeActiveClass(btn);
-            btn.parentElement.classList.remove("show");
+        bullets.forEach((btn) => {
+            removeClass(btn, "active");
+            removeClass(btn.parentElement, "active");
+            bulletsHolder.classList.contains("show") ? removeClass(bulletsHolder, "show") : false;
+            filter.classList.contains("active") ? removeClass(filter, "active") : false;
         });
         e.target.classList.add("active");
         e.target.parentElement.classList.add("active");
@@ -20,13 +40,60 @@ filterButtons.forEach((button) => {
     });
 });
 
-document.querySelector(".filter").onclick = function () {
-    filterButtons.forEach((btn) => {
-        btn.parentElement.classList.toggle("show");
-    })
+// Handle Clicking on Filter Button
+filter.onclick = function () {
+    this.classList.toggle("active");
+    bulletsHolder.classList.toggle("show");
 }
 
-// Get Post Function
+// <-- Start Functions -->
+
+// Handle Blog Search Function
+function handleBlogSearch() {
+    fetch("json/posts.json")
+    .then((result) => result.json())
+    .then((posts) => {
+        if (posts.length > 0) {
+            searchResultBox.innerHTML = "";
+
+            let searchValue = searchInput.value.toLowerCase();
+            let currentPost;
+            let pickedPosts = [];
+            
+            // Checking The Valid Post
+            for (let i = 0; i < posts.length; i++) { 
+                currentPost = posts[i];
+                if (pickedPosts.length === 4) {
+                    break;
+                }
+                for (let j = 0; j < currentPost.keywords.length; j++) {
+                    let currentPostKeywords = currentPost.keywords[j].toLowerCase();
+
+                    if (currentPostKeywords.includes(searchValue)) {
+                        pickedPosts.push(currentPost);
+                        break;
+                    }
+                }
+            }
+            // Creating Search Result
+            pickedPosts.forEach((post) => {
+                let li = document.createElement("li");
+                let a = document.createElement("a");
+
+                a.className = "d-block ps-3 py-3";
+                a.href = post.postLink;
+                a.appendChild(document.createTextNode(post.title));
+
+                li.appendChild(a);
+                searchResultBox.appendChild(li);
+            });
+        } else {
+            // If There is no Posts
+        }
+    });
+}
+
+// Get Blog Posts Function
 function getBlogPosts(category) {
     fetch("json/posts.json")
     .then((result) => result.json())
@@ -45,35 +112,39 @@ function getBlogPosts(category) {
     .then((filtredPosts) => {
         // Set Posts in The Page
         postsHolder.innerHTML = "";
-        for (let i = 0; i < filtredPosts.length; i++) {
-            let gridDiv = document.createElement("div");
-            let postBox = document.createElement("a");
-            let postImg = document.createElement("img");
-            let postCategory = document.createElement("span");
-            let postTitle = document.createElement("h3");
-
-            gridDiv.className = "col-md-6 col-lg-4";
-            postBox.className = "post-box d-block mx-auto mx-md-0 p-2";
-            postImg.className = "w-100";
-            postCategory.className = "post-category d-block mt-3 mb-1 ps-1 text-uppercase";
-            postTitle.className = "post-title fs-5 ps-1 mb-0";
-
-            postBox.href = filtredPosts[i].postLink;
-            postImg.src = filtredPosts[i].image;
-            postImg.alt = filtredPosts[i].title;
-            postCategory.innerHTML = filtredPosts[i].category;
-            postTitle.innerHTML = filtredPosts[i].title;
-
-            postBox.appendChild(postImg);
-            postBox.appendChild(postCategory);
-            postBox.appendChild(postTitle);
-            gridDiv.appendChild(postBox);
-            postsHolder.appendChild(gridDiv);
+        if (filtredPosts.length > 0) {
+            for (let i = 0; i < filtredPosts.length; i++) {
+                let gridDiv = document.createElement("div");
+                let postBox = document.createElement("a");
+                let postImg = document.createElement("img");
+                let postCategory = document.createElement("span");
+                let postTitle = document.createElement("h3");
+    
+                gridDiv.className = "col-md-6 col-lg-4";
+                postBox.className = "post-box d-block mx-auto mx-md-0 p-2";
+                postImg.className = "w-100";
+                postCategory.className = "post-category d-block mt-3 mb-1 ps-1 text-uppercase";
+                postTitle.className = "post-title fs-5 ps-1 mb-0";
+    
+                postBox.href = filtredPosts[i].postLink;
+                postImg.src = filtredPosts[i].image;
+                postImg.alt = filtredPosts[i].title;
+                postCategory.innerHTML = filtredPosts[i].category;
+                postTitle.innerHTML = filtredPosts[i].title;
+    
+                postBox.appendChild(postImg);
+                postBox.appendChild(postCategory);
+                postBox.appendChild(postTitle);
+                gridDiv.appendChild(postBox);
+                postsHolder.appendChild(gridDiv);
+            }
+        } else {
+            // If There is no Posts
         }
     });
 }
 
-// Remove Active Class Function
-function removeActiveClass(el) {
-    el.classList.remove("active");
+// Remove Class Function
+function removeClass(el, className) {
+    el.classList.remove(String(className));
 }
